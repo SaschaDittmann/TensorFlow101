020eacf53bac3215cc8b81a4401e1274f3b9446d
Sascha Dittmann <Sascha5812@gmx.de>
2018-11-10 03:11:35 +0100

    docker scripts updated

--- run-docker.sh
+++ run-docker.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 read -p 'Azure Subscription ID: ' subscription_id
-docker run -d -p 8888:8888 -p 6006:6006 -e "SUBSCRIPTION_ID=$(subscription_id)" --name tensorflow-101 -t bytesmith/tensorflow101
+docker run -d --rm -p 8888:8888 -p 6006:6006 -e "SUBSCRIPTION_ID=$subscription_id" --name tensorflow-101 -t bytesmith/tensorflow101
--- stop-docker.sh
+++ stop-docker.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
 docker stop tensorflow-101
-docker rm tensorflow-101
