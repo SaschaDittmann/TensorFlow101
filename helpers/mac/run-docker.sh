f19b3077d8799b3f1aecffd726dd28d755b92d8f
Sascha Dittmann <Sascha5812@gmx.de>
2018-12-02 14:13:16 +0100

    Update run-docker.sh

--- helpers/mac/run-docker.sh
+++ helpers/mac/run-docker.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
-read -p 'Azure Subscription ID: ' subscription_id
-docker run -d --rm -p 8888:8888 -p 6006:6006 -e "SUBSCRIPTION_ID=$subscription_id" --name tensorflow-101 -t bytesmith/tensorflow101
+AZ_SUBSCRIPTION_ID_DEFAULT=
+AZ_RESOURCE_GROUP_DEFAULT=tensorflow101
+AZ_REGION_DEFAULT=westeurope
+
+read -p "Azure Subscription ID [$AZ_SUBSCRIPTION_ID_DEFAULT]: " AZ_SUBSCRIPTION_ID
+AZ_SUBSCRIPTION_ID=${AZ_SUBSCRIPTION_ID:-$AZ_SUBSCRIPTION_ID_DEFAULT}
+
+read -p "Azure Resource Group [$AZ_RESOURCE_GROUP_DEFAULT]: " AZ_RESOURCE_GROUP
+AZ_RESOURCE_GROUP=${AZ_RESOURCE_GROUP:-$AZ_RESOURCE_GROUP_DEFAULT}
+
+read -p "Azure Region [$AZ_REGION_DEFAULT]: " AZ_REGION
+AZ_REGION=${AZ_REGION:-$AZ_REGION_DEFAULT}
+
+docker run -d --rm \
+    -p 8888:8888 -p 6006:6006 \
+    -e "SUBSCRIPTION_ID=$AZ_SUBSCRIPTION_ID" \
+    -e "RESOURCE_GROUP=$AZ_RESOURCE_GROUP" \
+    -e "WORKSPACE_REGION=$AZ_REGION" \
+    --name tensorflow-101 \
+    -t bytesmith/tensorflow101
